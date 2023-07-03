ec67dd32ebe438f9e7e0dec64f735049e15f6bd6
DvH <88995192+DangVietH@users.noreply.github.com>
2023-07-03 14:35:12 +0700

    forgot to add content

--- cogs/config/configuration.py
+++ cogs/config/configuration.py
@@ -357,17 +357,17 @@ class Configuration(commands.Cog):
 
         await msg.edit(content=f"Alright, the message will be sent to {channel.mention}.\nNow let's setup the embed message. What will be the title of the embed?")
         embed_title = await self.bot.wait_for('message', check=check)
-        embed = discord.Embed(title=inspect.cleandoc(embed_title)).set_footer(text="This is what the embed will look like")
+        embed = discord.Embed(title=embed_title.content).set_footer(text="This is what the embed will look like")
         await ctx.channel.purge(limit=1)
 
         await msg.edit(content="What will be the embed description", embed=embed)
         embed_desc = await self.bot.wait_for('message', check=check)
-        embed.description = inspect.cleandoc(embed_desc)
+        embed.description = embed_desc.content
         await ctx.channel.purge(limit=1)
 
         await msg.edit(content="What will be the embed color (Hex color only. If you want no color, type `skip`)", embed=embed)
         color_hex = await self.bot.wait_for('message', check=check)
-        if color_hex.lower() == "skip":
+        if color_hex.content.lower() == "skip":
             hex_val = 0
         else:
             hex_val = color_hex
